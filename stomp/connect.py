3a0ebecdab3351a7fc530c9ae147f6192bcc7fbf
Jason R Briggs <jasonrbriggs@gmail.com>
2013-10-01 23:38:05 +0100

    remove heartbeat from protocol11 (replaced by heartbeat listener). add protocol 1.0

--- stomp/connect.py
+++ stomp/connect.py
@@ -2,6 +2,35 @@ from transport import *
 from protocol import *
 
 
+class StompConnection10(Transport, Protocol10):
+    def __init__(self, 
+                 host_and_ports = [ ('localhost', 61613) ], 
+                 prefer_localhost = True,
+                 try_loopback_connect = True,
+                 reconnect_sleep_initial = 0.1,
+                 reconnect_sleep_increase = 0.5,
+                 reconnect_sleep_jitter = 0.1,
+                 reconnect_sleep_max = 60.0,
+                 reconnect_attempts_max = 3,
+                 use_ssl = False,
+                 ssl_key_file = None,
+                 ssl_cert_file = None,
+                 ssl_ca_certs = None,
+                 ssl_cert_validator = None,
+                 wait_on_receipt = False,
+                 ssl_version = DEFAULT_SSL_VERSION,
+                 timeout = None,
+                 keepalive = None):
+        Transport.__init__(self, host_and_ports, prefer_localhost, try_loopback_connect, 
+            reconnect_sleep_initial, reconnect_sleep_increase, reconnect_sleep_jitter, reconnect_sleep_max, reconnect_attempts_max,
+            use_ssl, ssl_key_file, ssl_cert_file, ssl_ca_certs, ssl_cert_validator, wait_on_receipt, ssl_version, timeout, keepalive, None)
+        Protocol10.__init__(self, self)
+        
+    def disconnect(self, receipt = None, headers = {}, **keyword_headers):
+        Protocol10.disconnect(self, receipt, headers, **keyword_headers)
+        Transport.stop(self)
+
+
 class StompConnection11(Transport, Protocol11):
     def __init__(self, 
                  host_and_ports = [ ('localhost', 61613) ], 
@@ -26,7 +55,7 @@ class StompConnection11(Transport, Protocol11):
         Transport.__init__(self, host_and_ports, prefer_localhost, try_loopback_connect, 
             reconnect_sleep_initial, reconnect_sleep_increase, reconnect_sleep_jitter, reconnect_sleep_max, reconnect_attempts_max,
             use_ssl, ssl_key_file, ssl_cert_file, ssl_ca_certs, ssl_cert_validator, wait_on_receipt, ssl_version, timeout,
-            heartbeats, keepalive, vhost)
+            keepalive, vhost)
         Protocol11.__init__(self, self, heartbeats)
         
     def disconnect(self, receipt = None, headers = {}, **keyword_headers):
@@ -58,7 +87,7 @@ class StompConnection12(Transport, Protocol12):
         Transport.__init__(self, host_and_ports, prefer_localhost, try_loopback_connect, 
             reconnect_sleep_initial, reconnect_sleep_increase, reconnect_sleep_jitter, reconnect_sleep_max, reconnect_attempts_max,
             use_ssl, ssl_key_file, ssl_cert_file, ssl_ca_certs, ssl_cert_validator, wait_on_receipt, ssl_version, timeout,
-            heartbeats, keepalive, vhost)
+            keepalive, vhost)
         Protocol12.__init__(self, self, heartbeats)
         
     def disconnect(self):
