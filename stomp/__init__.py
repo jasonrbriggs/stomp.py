4ee900953b6d37404befc9b81bbb331e59c41d14
Jason R Briggs <jasonrbriggs@gmail.com>
2015-03-22 11:47:27 +0000

    update version

--- stomp/__init__.py
+++ stomp/__init__.py
@@ -11,7 +11,7 @@ Project Page: https://github.com/jasonrbriggs/stomp.py
 
 import stomp.connect as connect, stomp.listener as listener
 
-__version__ = (4, 0, 13)
+__version__ = (4, 0, 14)
 
 ##
 # Alias for STOMP 1.0 connections.
