216341a058d643853918d55c124421a4b35a1ce2
Jason R Briggs <jasonrbriggs@gmail.com>
2015-12-31 10:41:55 +0000

    remove unicode prefix as it causes problems in python3.2

--- stomp/test/p3_backward_test.py
+++ stomp/test/p3_backward_test.py
@@ -4,10 +4,10 @@ from stomp import backward3
 
 class TestBackward3(unittest.TestCase):
     def test_pack_mixed_string_and_bytes(self):
-        lines = ['SEND', '\n', u'header1:test', u'\u6771']
+        lines = ['SEND', '\n', 'header1:test', '\u6771']
         self.assertEqual(backward3.encode(backward3.pack(lines)),
             b'SEND\nheader1:test\xe6\x9d\xb1')
-        lines = ['SEND', '\n', u'header1:test', b'\xe6\x9d\xb1']
+        lines = ['SEND', '\n', 'header1:test', b'\xe6\x9d\xb1']
         self.assertEqual(backward3.encode(backward3.pack(lines)),
             b'SEND\nheader1:test\xe6\x9d\xb1')
 
@@ -16,6 +16,6 @@ class TestBackward3(unittest.TestCase):
         self.assertEquals('test', backward3.decode(b'test'))
 
     def test_encode(self):
-        self.assertEquals(b'test', backward3.encode(u'test'))
+        self.assertEquals(b'test', backward3.encode('test'))
         self.assertEquals(b'test', backward3.encode(b'test'))
         self.assertRaises(TypeError, backward3.encode, None)
