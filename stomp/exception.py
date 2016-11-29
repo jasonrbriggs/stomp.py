56613350a284e74105a6bbe8f1e5c46daca63c0d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-29 14:03:23 +0000

    Add a common exception class so I can catch all stomp.py exceptions in one go.

--- stomp/exception.py
+++ stomp/exception.py
@@ -1,30 +1,35 @@
 """Errors thrown by stomp.py connections.
 """
 
+class StompException(Exception):
+    """
+    Common exception class. All specific stomp.py exceptions are subclasses
+    of StompException, allowing the library user to catch all current and
+    future library exceptions.
+    """
 
-class ConnectionClosedException(Exception):
+
+class ConnectionClosedException(StompException):
     """
     Raised in the receiver thread when the connection has been closed
     by the server.
     """
-    pass
 
 
-class NotConnectedException(Exception):
+class NotConnectedException(StompException):
     """
     Raised when there is currently no server connection.
     """
-    pass
 
 
-class ConnectFailedException(Exception):
+class ConnectFailedException(StompException):
     """
     Raised by Connection.attempt_connection when reconnection attempts
     have exceeded Connection.__reconnect_attempts_max.
     """
 
 
-class InterruptedException(Exception):
+class InterruptedException(StompException):
     """
     Raised by receive when data read is interrupted.
     """
