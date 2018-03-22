e024b60a952d7c52f2ad435d4bdad83125bec407
Tuomas Virtanen <tuomas.virtanen@qvantel.com>
2018-03-22 16:28:07 +0200

    Allow custom headers in nack calls
    Eg. RabbitMQ allows custom headers in nack messages (eg. requeue).

--- stomp/test/rabbitmq_test.py
+++ stomp/test/rabbitmq_test.py
@@ -25,3 +25,35 @@ class TestRabbitMQSend(unittest.TestCase):
         self.assertTrue(listener.connections == 1, 'should have received 1 connection acknowledgement')
         self.assertTrue(listener.messages == 1, 'should have received 1 message')
         self.assertTrue(listener.errors == 0, 'should not have received any errors')
+
+    def test_clientnack_requeue(self):
+        conn = stomp.Connection11(get_rabbitmq_host())
+        listener = TestListener('123')
+        conn.set_listener('', listener)
+        conn.connect(get_rabbitmq_user(), get_rabbitmq_password(), wait=True)
+        queuename = '/queue/testnack'
+        conn.subscribe(destination=queuename, id=1, ack='client')
+        conn.send(body='this is a test', destination=queuename, receipt='123')
+
+        # Get the sent message
+        listener.wait_for_message()
+        (headers, _) = listener.get_latest_message()
+        message_id = headers['message-id']
+        subscription = headers['subscription']
+        self.assertTrue(listener.messages == 1, 'should have received 1 message')
+
+        # Nack with requeue=True, now the message should be again waiting in the queue
+        conn.nack(message_id, subscription, requeue=True)
+
+        # Get the requeued message
+        listener.wait_for_message()
+        (headers, _) = listener.get_latest_message()
+        message_id = headers['message-id']
+        subscription = headers['subscription']
+        self.assertTrue(listener.messages == 2, 'should have received 2 messages')
+
+        # Clean up
+        conn.nack(message_id, subscription, requeue=False)
+        listener.wait_on_receipt()
+        conn.disconnect(receipt=None)
+        self.assertTrue(listener.errors == 0, 'should not have received any errors')
