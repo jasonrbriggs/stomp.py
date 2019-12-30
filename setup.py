a1e72dc96f1bf0a2ed9c6c92c7428cc93c74a93d
Jason R Briggs <mail@jasonrbriggs.com>
2019-12-30 16:32:57 +0000

    fix classifiers

--- setup.py
+++ setup.py
@@ -125,7 +125,6 @@ setup(
          'Development Status :: 5 - Production/Stable',
          'Intended Audience :: Developers',
          'License :: OSI Approved :: Apache Software License',
-         'Programming Language :: Python :: 2',
          'Programming Language :: Python :: 3'
     ]
 )
