1805d74cd9656911fcd35774c753dd9fca5df139
Jason R Briggs <mail@jasonrbriggs.com>
2020-01-18 10:02:38 +0000

    add another assert message

--- tests/test_ss.py
+++ tests/test_ss.py
@@ -136,7 +136,7 @@ content-type:text/plain
         listener.wait_for_message()
         headers, body = listener.get_latest_message()
 
-        assert expected_heartbeat_count == listener.heartbeat_count
+        assert expected_heartbeat_count == listener.heartbeat_count, "(1) expected heartbeat count %s, was %s" % (expected_heartbeat_count, listener.heartbeat_count)
         assert {"content-type": "text/plain"} == headers
         assert message_body == body
 
@@ -153,7 +153,7 @@ content-type:text/plain
         listener.wait_for_heartbeat()
         headers, body = listener.get_latest_message()
 
-        assert expected_heartbeat_count == listener.heartbeat_count, "expected heartbeat count %s, was %s" % (expected_heartbeat_count, listener.heartbeat_count)
+        assert expected_heartbeat_count == listener.heartbeat_count, "(2) expected heartbeat count %s, was %s" % (expected_heartbeat_count, listener.heartbeat_count)
         assert {"content-type": "text/plain"} == headers
         assert message_body == body
 
